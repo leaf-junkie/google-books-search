e06245c9d01b7f6f03ba47138257a13d69369587
leaf-junkie <ragsdale.jar@gmail.com>
2019-05-05 21:06:27 -0400

    Beginning to code frontend

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./style.css"
+
+const Footer = () => {
+    return (
+        <footer className="bg-light text-secondary text-center">
+            <p><a href="https://github.com/leaf-junkie/google-books-search" target="_blank">Julie Ragsdale</a> | 2019</p>
+        </footer>
+    );
+} 
+
+export default Footer;
--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,5 +1,20 @@
 const db = require("../models/book");
 
 module.exports = {
-    // 
+    // Display all Saved books
+    findAll: function(req, res) {
+        db.find({})
+        .then(bookModel => res.json(bookModel))
+        .catch(err => res.status(422).json(err));
+    },
+
+    // Add a new book to Saved?
+    create: function(req, res) {
+
+    },
+
+    // Remove a book from Saved based on ID
+    remove: function(req, res) {
+
+    }
 }
