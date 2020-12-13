f7076f7555487e11afa421a310cb9192a8c9e708
leaf-junkie <ragsdale.jar@gmail.com>
2020-12-12 20:38:40 -0500

    Disable nav link to saved page, as Heroku no longer supports the mLab add-on

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -10,8 +10,7 @@ const Footer = () => {
             <br></br>
             <footer className="bg-light text-secondary text-center">
                 <p>
-                    <a href="https://github.com/leaf-junkie/google-books-search" target="_blank">Julie Ragsdale</a> 
-                    | <script>{document.write(new Date().getFullYear())}</script>
+                    <a href="https://github.com/leaf-junkie/google-books-search" target="_blank">Julie Ragsdale</a> | {(new Date().getFullYear())}
                 </p>
             </footer>
         </div>
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,16 +4,17 @@ import "./style.css";
 const Nav = () => {
     return (
         <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand" href="/">
+            <a className="navbar-brand" href="#">
                 <img src={require("./books-solid.svg")} width="25" ></img>
                 The Shelf
             </a>
             <ul className="navbar-nav">
                 <li className="nav-item">
-                    <a className="nav-link" href="/">Search </a>
+                    <a className="nav-link" href="#">Search</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/books">Saved </a>
+                    <a className="nav-link disabled" href="" type="button">Saved</a>
+                    {/* <a className="nav-link" href="/books">Saved</a> */}
                 </li>
             </ul>            
         </nav>
